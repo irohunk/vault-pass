73447409eade6ac8becb831ae7cce52badfa492e
Rohan Kulkarni <irohunk@gmail.com>
2024-07-29 17:17:42 -0400

    added wireframe and erd

---
+++ db/migrations/db_tables.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS organizations CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS websites CASCADE;
+
+CREATE TABLE organizations (
+  id PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  postal VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users (
+  id PRIMARY KEY NOT NULL,
+  org_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE,
+  is_admin BOOLEAN NOT NULL,
+  login_username VARCHAR(255) NOT NULL,
+  login_password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE websites (
+  id PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  org_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE,
+  url TEXT NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
